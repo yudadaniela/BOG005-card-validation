8a88f669e1663d5911798ead7b93bed49d90b934
Daniela Nieto <yudadaniela@hotmail.com>
2022-07-18 18:51:18 -0500

    cambiosuno

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
+const boton = document.querySelectorAll('button');
+
+boton[0].addEventListener('click', function (){
+    alert("funciono");
+    let ccnumber = document.getElementById("number").value;
+    alert(ccnumber);
+    console.log(validator['isValid'](ccnumber))
+})
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,40 @@
-const validator = {
-  // ...
+const validator =  {
+   // ...FUNCION ISVALID
+   
+   isValid : (ccnumber) =>{
+    const numdig = ccnumber.length;
+    let suma = 0;
+    const total = 0;
+    var result = false;
+    
+    if (numdig>14 && numdig<=16) {
+       
+       for(let i = 15; i>=0; i--){
+        let vectorinver = parseInt(ccnumber[i]);
+      }
+
+      for(let j=0; j<numdig; j++) {
+        if (j%2 === 0){
+          vectorinver*=2;
+        }
+          if (vectorinver[j]>9) { //verificar si  esta escrito correctamente 
+            vectorinver -=9;  //verificar si esta escrito correctamente 
+          }    
+        suma += vectorinver
+      }
+    } else { 
+      result = false;
+    }
+    total = suma % 10;
+    if (total===0) {
+      result = true;
+    } else { 
+      result = false;
+    }
+    return result;
+  }
+  // ...FUNCION MASCARA
 };
+
 
 export default validator;
